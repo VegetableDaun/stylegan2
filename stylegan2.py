fc24a1f2473a7a411d7891696b6428f4427718a4
Kirill <kirillilenkov974@gmail.com>
2023-11-06 17:32:56 +0300

    added CategoricalCrossentropy

--- stylegan2.py
+++ stylegan2.py
@@ -43,7 +43,7 @@ class StyleGan2(tf.keras.Model):
         self.discriminator = StyleGan2Discriminator(resolution=self.resolution, weights=weights,
                                                     impl=impl, gpu=gpu, name='Discriminator')
 
-    def call(self, latent_vector=1, c=None):
+    def call(self, latent_vector, c=None):
         """
         Parameters
         ----------
@@ -56,7 +56,7 @@ class StyleGan2(tf.keras.Model):
 
         assert c is not None, "Use conditional"
 
-        img = self.generator(latent_vector, lambda_t=latent_vector, c=c)
+        img = self.generator(latent_vector, lambda_t=1, c=c)
         score = self.discriminator(img, c=c)
 
         return score
@@ -76,6 +76,8 @@ class StyleGan2(tf.keras.Model):
 
         self.d_loss_metric = keras.metrics.Mean(name="d_loss")
         self.g_loss_metric = keras.metrics.Mean(name="g_loss")
+
+        self.CategoricalCrossentropy = tf.keras.losses.CategoricalCrossentropy()
 
         super().compile(*args, **kwargs)
 
@@ -109,7 +111,7 @@ class StyleGan2(tf.keras.Model):
             fake_images = self.generator(noise, lambda_t=self.lambda_t(self.epoch), c=one_hot_labels)
             pred_fake = self.discriminator(fake_images, c=one_hot_labels)
             g_loss = (self.wasserstein_loss(real_labels, pred_fake[0])
-                      + self.lambda_t(self.epoch) * pred_fake[1])
+                      + self.lambda_t(self.epoch) * self.CategoricalCrossentropy(one_hot_labels, pred_fake[1]))
 
             trainable_weights = (self.generator.mapping_network.trainable_weights
                                  + self.generator.synthesis_network.trainable_weights)
@@ -129,13 +131,13 @@ class StyleGan2(tf.keras.Model):
 
             # calculate losses
             loss_fake = (self.wasserstein_loss(fake_labels, pred_fake[0])
-                             + self.lambda_t(self.epoch) * pred_fake[1])
+                             + self.lambda_t(self.epoch) * self.CategoricalCrossentropy(one_hot_labels, pred_fake[1]))
 
             loss_real = (self.wasserstein_loss(real_labels, pred_real[0])
-                             + self.lambda_t(self.epoch) * pred_real[1])
+                             + self.lambda_t(self.epoch) * self.CategoricalCrossentropy(one_hot_labels, pred_fake[1]))
 
             loss_fake_grad = (self.wasserstein_loss(real_labels, pred_fake_grad[0])
-                             + self.lambda_t(self.epoch) * pred_fake_grad[1])
+                             + self.lambda_t(self.epoch) * self.CategoricalCrossentropy(one_hot_labels, pred_fake[1]))
 
 
             # gradient penalty
@@ -143,19 +145,15 @@ class StyleGan2(tf.keras.Model):
             gradient_penalty = self.loss_weights["gradient_penalty"] * self.gradient_loss(gradients_fake)
 
             # drift loss
-            new_pred_fake = pred_fake[0] + self.lambda_t(self.epoch) * pred_fake[1]
-            new_pred_real = pred_real[0] + self.lambda_t(self.epoch) * pred_real[1]
-            all_pred = tf.concat([new_pred_fake, new_pred_real], axis=0)
-            drift_loss = self.loss_weights["drift"] * tf.reduce_mean(all_pred ** 2)
+            # new_pred_fake = pred_fake[0] + self.lambda_t(self.epoch) * pred_fake[1]
+            # new_pred_real = pred_real[0] + self.lambda_t(self.epoch) * pred_real[1]
+            # all_pred = tf.concat([new_pred_fake, new_pred_real], axis=0)
+            # drift_loss = self.loss_weights["drift"] * tf.reduce_mean(all_pred ** 2)
 
-            d_loss = loss_fake + loss_real + gradient_penalty + drift_loss
+            d_loss = loss_fake + loss_real + gradient_penalty #+ drift_loss
 
             gradients = total_tape.gradient(d_loss, self.discriminator.trainable_weights)
             self.d_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_weights))
-
-        if self.epoch == 30:
-            self.generator.save("GEN_30.npy")
-            self.discriminator.save("DIS_30.npy")
 
         # Update epoch
         self.epoch += 1
