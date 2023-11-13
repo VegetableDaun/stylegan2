a34ab1eedffc51c8bf6aad49de47bbae9a1486e3
Kirill <kirillilenkov974@gmail.com>
2023-11-13 18:53:40 +0300

    added callback

---
+++ CustomCallback.py
@@ -0,0 +1,81 @@
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+from PIL import Image
+from tensorflow import keras
+
+from config_GAN import path_to_result, path_to_noise, path_to_discriminator, path_to_generator
+
+
+class CustomCallback_epoch(keras.callbacks.Callback):
+    def on_epoch_begin(self, epoch, logs=None):
+        self.model.epoch += 1
+
+
+class CustomCallback_save(keras.callbacks.Callback):
+    def __init__(self, num_save=5, save_last=True, path=Path(path_to_result), noise=Path(path_to_noise)):
+        super(CustomCallback_save, self).__init__()
+        self.num_save = num_save
+        self.path = path
+        self.save_last = save_last
+
+        try:
+            os.makedirs(self.path)
+        except:
+            pass
+
+        if not os.path.isfile(self.path / 'data.json'):
+            self.counter = 0
+        else:
+            with open(self.path / 'data.json', 'r') as F:
+                self.counter = int(json.load(F))
+
+        with open(noise, mode='r') as F:
+            self.noise = np.array(json.load(F))
+
+    def on_train_begin(self, logs=None):
+        if os.path.isfile(self.path / 'metrics.json'):
+            self.counter -= self.counter % self.num_save
+
+        if self.counter == 0:
+            os.remove(self.path / 'metrics.json')
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.counter += 1
+        if not os.path.isfile(self.path / 'metrics.json'):
+            with open(self.path / 'metrics.json', mode='w') as F:
+                json.dump({self.counter: logs}, F)
+        else:
+            with open(self.path / 'metrics.json', mode='r') as F:
+                feeds = dict(json.load(F))
+                feeds[str(self.counter)] = logs
+            with open(self.path / 'metrics.json', mode='w') as F:
+                json.dump(feeds, F)
+
+        if (epoch + 1) % self.num_save == 0:
+            try:
+                os.makedirs(self.path / 'Image')
+                os.makedirs(self.path / path_to_discriminator)
+                os.makedirs(self.path / path_to_generator)
+            except:
+                pass
+
+            if self.save_last:
+                self.model.discriminator.save(self.path / 'Discriminator.hdf5')
+                self.model.generator.save(self.path / 'Generator.hdf5')
+            else:
+                self.model.discriminator.save(self.path / path_to_discriminator / f'Discriminator_{self.counter}.hdf5')
+                self.model.generator.save(self.path / path_to_generator / f'Generator_{self.counter}.hdf5')
+
+            img = np.array(self.model.generator(self.noise))[0]
+            img = np.round(img * 255)
+            img = img.astype(np.uint8)
+            if img.shape[2] == 1:
+                img = np.dstack([img, img, img])
+            img = Image.fromarray(img)
+            img.save(self.path / ('Image/Epoch_' + f'{self.counter}.jpg'))
+
+        with open(self.path / 'data.json', 'w') as F:
+            json.dump(self.counter, F)
--- stylegan2.py
+++ stylegan2.py
@@ -79,8 +79,9 @@ class StyleGan2(tf.keras.Model):
 
         super().compile(*args, **kwargs)
 
-    def lambda_t(self, epoch):
-        return min([max([(epoch - self.T_s) / (self.T_e - self.T_s), 0]), 1])
+    @property
+    def lambda_t(self):
+        return min([max([(self.epoch - self.T_s) / (self.T_e - self.T_s), 0]), 1])
 
     @staticmethod
     def wasserstein_loss(y_true, y_pred):
@@ -106,11 +107,11 @@ class StyleGan2(tf.keras.Model):
 
         # generator
         with tf.GradientTape() as g_tape:
-            fake_images = self.generator(noise, lambda_t=self.lambda_t(self.epoch), c=one_hot_labels)
+            fake_images = self.generator(noise, lambda_t=self.lambda_t, c=one_hot_labels)
             pred_fake = self.discriminator(fake_images, c=one_hot_labels)
             g_loss = (self.wasserstein_loss(real_labels, pred_fake[0])
-                      + self.lambda_t(self.epoch) * self.wasserstein_loss(real_labels, pred_fake[1]))
-                      # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake[2]))
+                      + self.lambda_t * self.wasserstein_loss(real_labels, pred_fake[1]))
+            # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake[2]))
 
             trainable_weights = (self.generator.mapping_network.trainable_weights
                                  + self.generator.synthesis_network.trainable_weights)
@@ -130,24 +131,24 @@ class StyleGan2(tf.keras.Model):
 
             # calculate losses
             loss_fake = (self.wasserstein_loss(fake_labels, pred_fake[0])
-                         + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake[1]))
-                         # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake[2]))
+                         + self.lambda_t * self.wasserstein_loss(fake_labels, pred_fake[1]))
+            # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake[2]))
 
             loss_real = (self.wasserstein_loss(real_labels, pred_real[0])
-                         + self.lambda_t(self.epoch) * self.wasserstein_loss(real_labels, pred_real[1]))
-                         # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_real[2]))
+                         + self.lambda_t * self.wasserstein_loss(real_labels, pred_real[1]))
+            # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_real[2]))
 
             loss_fake_grad = (self.wasserstein_loss(real_labels, pred_fake_grad[0])
-                              + self.lambda_t(self.epoch) * self.wasserstein_loss(real_labels, pred_fake_grad[1]))
-                              # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake_grad[2]))
+                              + self.lambda_t * self.wasserstein_loss(real_labels, pred_fake_grad[1]))
+            # + self.lambda_t(self.epoch) * self.wasserstein_loss(fake_labels, pred_fake_grad[2]))
 
             # gradient penalty
             gradients_fake = gradient_tape.gradient(loss_fake_grad, [interpolates])
             gradient_penalty = self.loss_weights["gradient_penalty"] * self.gradient_loss(gradients_fake)
 
             # drift loss
-            new_pred_fake = pred_fake[0] + self.lambda_t(self.epoch) * pred_fake[1] # + pred_fake[2])
-            new_pred_real = pred_real[0] + self.lambda_t(self.epoch) * pred_real[1] # + pred_real[2])
+            new_pred_fake = pred_fake[0] + self.lambda_t * pred_fake[1]  # + pred_fake[2])
+            new_pred_real = pred_real[0] + self.lambda_t * pred_real[1]  # + pred_real[2])
             all_pred = tf.concat([new_pred_fake, new_pred_real], axis=0)
             drift_loss = self.loss_weights["drift"] * tf.reduce_mean(all_pred ** 2)
 
@@ -155,9 +156,6 @@ class StyleGan2(tf.keras.Model):
 
             gradients = total_tape.gradient(d_loss, self.discriminator.trainable_weights)
             self.d_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_weights))
-
-        # Update epoch
-        self.epoch += 1
 
         # Update metrics
         self.d_loss_metric.update_state(d_loss)
