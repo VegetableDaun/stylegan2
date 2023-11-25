5bee0025759d71306b5fcdee4827ddc2ea384614
Kirill <kirillilenkov974@gmail.com>
2023-11-25 21:18:37 +0300

    mda

--- CustomCallback.py
+++ CustomCallback.py
@@ -17,11 +17,9 @@ class CustomCallback_epoch(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         opt_cfg = {"learning_rate": 1e-3, "beta_1": 0.0, "beta_2": 0.99, "epsilon": 1e-8}
 
-        print(f'self.model.epoch = {self.model.epoch}', f'self.model.T_e = {self.model.T_e}')
-        
         if self.model.epoch == self.model.T_e:
-            self.model.d_optimizer = keras.optimizers.legacy.Adam(**opt_cfg)
-            self.model.g_optimizer = keras.optimizers.legacy.Adam(**opt_cfg)
+            self.model.d_optimizer = tf.keras.optimizers.legacy.Adam(**opt_cfg)
+            self.model.g_optimizer = tf.keras.optimizers.legacy.Adam(**opt_cfg)
 
 
 class CustomCallback_save(keras.callbacks.Callback):
